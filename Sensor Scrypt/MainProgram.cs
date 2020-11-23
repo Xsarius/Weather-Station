55260853043c7fd46dea3315fa3716cbd438473b
Xsarius <60043296+Xsarius@users.noreply.github.com>
2020-11-23 11:40:31 +0100

    1.0

---
+++ Sensor Scrypt/MainProgram.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace Sensor_Scrypt
+{
+    class MainProgram
+    {
+
+        static void Main(string[] args)
+        {
+            // Sensor declarations
+
+            Sensor thermometer = new Sensor();
+            thermometer.adress = 1; // Sensor adress, specified for every sensor in datasheet(hex)
+
+            /*
+            Sensor sensor2 = new Sensor();
+            Sensor sensor3 = new Sensor();
+            .
+            .
+            .
+            */
+
+            Sensor[] devices = { thermometer /*, sensor 1, sensor 2, sensor 3, etc.*/ }; //Type in all sensors
+
+            string filePath = @"C:\\\Downloads\test1.txt";
+
+            StreamWriter FileStream = File.CreateText(filePath); // Create/open file in file path
+
+            foreach (var sensor in devices)
+            {
+                FileStream.Write(sensor.DataOutput());  // Writing data to file
+            }
+
+        }
+
+    }
+}
---
+++ Sensor Scrypt/Sensor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Windows.Devices.I2c;
+
+namespace Sensor_Scrypt
+{
+    public class Sensor
+    {
+        // Adress of the sensor
+        public int adress;
+
+        // Data outputed by the sensor
+        public float data;
+        // This device 
+        I2cDevice device;
+
+        public async void OpenConnection()
+        {
+            // this.sensorAdress is the I2C device address
+            var settings = new I2cConnectionSettings(this.adress);
+
+            // FastMode = 400KHz
+            settings.BusSpeed = I2cBusSpeed.FastMode;
+
+            // Create an I2cDevice with the specified I2C settings
+            var controller = await I2cController.GetDefaultAsync();
+
+            // Create an I2cDevice with the specified I2C settings
+            device = controller.GetDevice(settings);
+        }
+
+        public float DataOutput()
+        {
+            // Buffer array
+            byte[] readBuff = { };
+
+            // Start transmission line with the sensor
+            OpenConnection();
+
+            // Read sensor output
+            using (device)
+            {
+                device.Read(readBuff);
+            }
+
+            //  BitConverter.ToSingle(byte[], int32) converts byte[] to float[] type
+            return BitConverter.ToSingle(readBuff, 0);
+        }
+
+    }
+}
